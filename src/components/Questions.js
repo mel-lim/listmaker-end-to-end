3c9201a2a8f869c46505c42ce48e48a27143581b
mel <43204513+mel-lim@users.noreply.github.com>
2021-03-22 14:53:53 -0700

    fix the way user question radio button wraps when window is resized

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 export const Questions = ({ setSelected }) => {
-    
+
 
     return (
         <section className="user-question">
             <h3 className="lighter-weight">Day trip or overnight trip?</h3>
             <form>
-                <input type="radio" id="day-trip-radio" name="day-or-overnight-radio" value="day-trip" className="radio-buttons" onClick={event => {setSelected(event.target.value)}} />
-                <label htmlFor="day-trip-radio">Day trip</label>
-                <input type="radio" id="overnight-trip-radio" name="day-or-overnight-radio" value="overnight"
-                    className="radio-buttons" onClick={event => {setSelected(event.target.value)}} />
-                <label htmlFor="overnight-trip-radio">Overnight trip</label>
+                <div>
+                    <input type="radio" id="day-trip-radio" name="day-or-overnight-radio" value="day-trip" className="radio-buttons" onClick={event => { setSelected(event.target.value) }} />
+                    <label htmlFor="day-trip-radio">Day trip</label>
+                </div>
+                <div>
+                    <input type="radio" id="overnight-trip-radio" name="day-or-overnight-radio" value="overnight"
+                        className="radio-buttons" onClick={event => { setSelected(event.target.value) }} />
+                    <label htmlFor="overnight-trip-radio">Overnight trip</label>
+                </div>
             </form>
 
         </section>
