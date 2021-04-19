d28869b92c3ccdde0cd73e861a904142a153b7d1
mel <43204513+mel-lim@users.noreply.github.com>
2021-04-18 20:57:57 -0700

    delete errant colon in confirm delete modal

--- src/components/Dashboard/ConfirmDeleteModal.js
+++ src/components/Dashboard/ConfirmDeleteModal.js
@@ -38,7 +38,7 @@ export const ConfirmDeleteModal = ({ activeTrip, toggleRefreshAllTripsDropdown, 
             {
                 close => (
                     <div className="confirm-delete-modal">
-                        <div className="modal-header">Are you sure you want to delete this trip?</div> :
+                        <div className="modal-header">Are you sure you want to delete this trip?</div>
                         <div className="actions">
                             <input type="button" value="Delete forever" onClick={deleteTrip} />
                             <input type="button" value="Keep for now" onClick={close} />
