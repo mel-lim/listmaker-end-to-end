a5efd78d6450e25a11d57f563fdbb93c26669f33
mel <43204513+mel-lim@users.noreply.github.com>
2021-05-06 17:11:40 -0700

    change undo icon based on user feedback

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ export const Footer = () => {
   return (
       <footer>
         <p>Credit, where credit is due.</p>
-        <p>UI icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel
-        perfect</a> and <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a></p>
+        <p>UI icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect" target="_blank" rel="noreferrer">Pixel
+        perfect</a> and <a href="https://www.flaticon.com/uicons" title="uicons" target="_blank" rel="noreferrer">Uicons</a> from <a href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="noreferrer"> www.flaticon.com</a></p>
         <p>&#169; mellim.io</p>
       </footer>
   );
